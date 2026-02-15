37bfc0ee856b70f699076841989148e0650c3542
ScriptiveMen <i.satyakumarram@gmail.com>
2026-02-15 16:46:04 +0530

    understand grouping, transformation, rotation, scaling

--- vite-setup/main.js
+++ vite-setup/main.js
@@ -10,20 +10,37 @@ const camera = new THREE.PerspectiveCamera(
 
 camera.position.z = 5;
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: "red", wireframe: true });
-const cube = new THREE.Mesh(geometry, material);
+const cubegeo = new THREE.BoxGeometry(1, 1, 1, 2, 2, 2);
+const cubemat = new THREE.MeshBasicMaterial({ color: "red", wireframe: true });
+const cube = new THREE.Mesh(cubegeo, cubemat);
 
+cube.position.x = -1;
 scene.add(cube);
+
+const spheregeo = new THREE.SphereGeometry(1, 40, 15);
+const spheremat = new THREE.MeshBasicMaterial({
+    color: "blue",
+    wireframe: true,
+});
+const sphere = new THREE.Mesh(spheregeo, spheremat);
+
+sphere.position.x = 1;
+scene.add(sphere);
 
 const canvas = document.querySelector("canvas");
 const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(window.innerWidth, window.innerHeight);
 
-function animate() {
-    window.requestAnimationFrame(animate);
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-    renderer.render(scene, camera);
-}
-animate();
+// rotation
+// cube.rotation.z = Math.PI - 0.1;
+
+// grouping
+const group = new THREE.Group();
+group.add(cube);
+group.add(sphere);
+
+// adding group into scene
+scene.add(group);
+group.position.x = -2;
+
+renderer.render(scene, camera);
