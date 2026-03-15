e8a69773c5e3d8cd9d769dc7ea46489520d7e784
ScriptiveMen <i.satyakumarram@gmail.com>
2026-03-15 20:49:27 +0530

    Intro to Fragment Shader

---
+++ glsl-fragmentShader/main.js
@@ -0,0 +1,35 @@
+import * as THREE from "three";
+import vertex from "./shaders/vertex.glsl";
+import fragment from "./shaders/fragment.glsl";
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    100,
+);
+
+camera.position.z = 5;
+
+const geometry = new THREE.BoxGeometry(2, 2);
+const material = new THREE.ShaderMaterial({
+    vertexShader: vertex,
+    fragmentShader: fragment,
+    uniforms: {
+        uTime: { value: 0 },
+    },
+});
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+const canvas = document.querySelector("canvas");
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+function animate() {
+    requestAnimationFrame(animate);
+    material.uniforms.uTime.value += 0.05;
+    renderer.render(scene, camera);
+}
+animate();
