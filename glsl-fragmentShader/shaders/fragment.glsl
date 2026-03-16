e3fec61a09026b140b6ed718cf8e83fed5fb96e0
ScriptiveMen <i.satyakumarram@gmail.com>
2026-03-16 13:58:46 +0530

    clamp() done

--- glsl-fragmentShader/shaders/fragment.glsl
+++ glsl-fragmentShader/shaders/fragment.glsl
@@ -4,13 +4,8 @@ uniform float uTime;
 void main() {
 
 
-    vec4 c1 = vec4(0.1686, 0.251, 0.8863, 1.0);
-    vec4 c2 = vec4(0.8941, 0.8706, 0.1843, 1.0);
+  float val= clamp(-1.,1.,vUv.x);
 
-    float num = step(.5, vUv.y);
-
-    vec4 final = mix(c1, c2, num);
-
-    gl_FragColor = final;
+    gl_FragColor = vec4(val,val,val,1.);
 
 }
